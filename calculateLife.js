5388ab5164c501bc73ea11c2104c22d8af2b3b82
tarepan <tarepan5884@gmail.com>
2018-06-29 17:44:53 +0900

    Add deathRate acquisition from e-stat

--- calculateLife.js
+++ calculateLife.js
@@ -1,21 +1,48 @@
-const fs = require("fs");
-const f = fs.readFileSync("./resource/deathRate_2015man.txt", {
-  encoding: "utf-8"
-});
-const deathRate = f.split("\r\n").filter(stg => stg != "").map(stg => parseFloat(stg));
-const startYear = 25; // になった人が
-const endYear = 50; // 歳になることができる確率
-const origin = deathRate.slice(startYear, endYear);
-console.log(origin);
-console.log(origin.reduce((total, rate) => total * (1 - rate), 1));
+(async () => {
+  let resjson = await getLifeTable();
+  let [men, women] =processDeathRates(resjson);
+  console.log(men);
+  console.log(women);
+})();
 
-const fetch = require("node-fetch");
+async function getLifeTable() {
+  const baseURL = "http://api.e-stat.go.jp/rest/2.1/app";
+  const appID = "6e52e69bb11b4255f91f693711fa7376c3c38026";
+  const format = "/json";
+  const action = "getStatsData";
+  const url = `${baseURL}${format}/${action}?appId=${appID}&lang=J&statsDataId=0003109558&metaGetFlg=Y&cntGetFlg=N&sectionHeaderFlg=1`;
+  let res = await fetch(url);
+  let resjson = await res.json();
+  return resjson
+}
 
-const appID = "6e52e69bb11b4255f91f693711fa7376c3c38026";
-let url = "http://api.e-stat.go.jp/rest/2.1/app/json/getStatsData?appId=" + appID + "&lang=J&statsDataId=0003109558&metaGetFlg=Y&cntGetFlg=N&sectionHeaderFlg=1"
-
-fetch(url)
-.then(async res=>{
-  let restxt = await res.text();
-  console.log(restxt);
-})
+function processDeathRates(estatJSON) {
+  const dataList = estatJSON.GET_STATS_DATA.STATISTICAL_DATA.DATA_INF.VALUE;
+  // console.log(dataList);
+  const deathRate = dataList.filter(datum => datum["@tab"] == "130");
+  const latest = deathRate.filter(datum => datum["@time"] == "2015000000");
+  const yearDeath = latest.filter(datum => parseInt(datum["@cat01"]) >= 300000);
+  const yearParsed = yearDeath.map(datum => {
+    datum["@cat01"] = parseInt(datum["@cat01"].slice(1, 4));
+    return datum
+  });
+  const men = yearParsed.filter(datum => datum["@cat02"] == "10").map(datum => {
+    let rd = {
+      published: parseInt(datum["@time"].slice(0, 4)),
+      sex: "man",
+      age: datum["@cat01"],
+      deathRate: datum["$"]
+    };
+    return rd
+  });
+  const women = yearParsed.filter(datum => datum["@cat02"] == "20").map(datum => {
+    let rd = {
+      published: parseInt(datum["@time"].slice(0, 4)),
+      sex: "woman",
+      age: datum["@cat01"],
+      deathRate: datum["$"]
+    };
+    return rd
+  });
+  return [men, women]
+}
